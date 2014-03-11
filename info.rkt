418132dad7bdc94726780c8d123b9ccab711a0c9
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-03-11 18:09:10 -0400

    Add Racket package deps.

--- info.rkt
+++ info.rkt
@@ -1,2 +1,11 @@
 #lang setup/infotab
 (define collection 'multi)
+(define deps '("rfc6455"
+	       ;; these are what the package system tells me I need:
+	       "base"
+	       "data-lib"
+	       "net-lib"
+	       "profile-lib"
+	       "rackunit-lib"
+	       "web-server-lib"
+	       ))
